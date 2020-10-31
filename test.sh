eac3597572b7b7028e30d325f2c6903fff67ee76
mindelly <flowper@yandex.ru>
2020-10-31 13:19:32 +0300

    deleted some files

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -c 10
-#SBATCH --output=out_%j.log
-#SBATCH --error=err_%j.log
-
-module load Python/Anaconda_v10.2019
-source activate flow
-python /home/aspotapova_2/test_for_more_eig.py
