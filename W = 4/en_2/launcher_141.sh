c58aeecdaf07557df5d8090740dd966a96e2e77e
pileyan <pileyan@users.noreply.github.com>
2023-12-29 17:35:34 +0300

    update

--- W = 4/en_2/launcher_141.sh
+++ W = 4/en_2/launcher_141.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=out_%j.log
 #SBATCH --error=err_%j.log
 #SBATCH --time=14-12
-#SBATCH --array=20,21,22,23,24,25,26,27
+#SBATCH --array=10,11,12,13,14,15,16,17,18,19
 
 # (Nx,Ny,Nup,Ndown,energy_file_name);
 # y=20
@@ -16,4 +16,4 @@
 NUP=`printf %d $SLURM_ARRAY_TASK_ID`
 NDOWN=`printf %d $SLURM_ARRAY_TASK_ID`
 
-./hubbard 40 4 1 $NUP $NDOWN -7.0 0 -5 1
+./hubbard 40 4 1 $NUP $NDOWN -7.0 0 -1 1
